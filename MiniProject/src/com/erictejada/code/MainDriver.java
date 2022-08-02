e3f5ece5fc3cbac2d3c95a09fd7847d17502f2c1
erictejada <eric173@revature.net>
2022-08-02 15:36:39 -0500

    new temp readings

--- MiniProject/src/com/erictejada/code/MainDriver.java
+++ MiniProject/src/com/erictejada/code/MainDriver.java
@@ -5,19 +5,45 @@ import java.util.Scanner;
 public class MainDriver {
 
 	public static void main(String[] args) {
-		Thermometer NewThermometer = new Thermometer(0,0,null, new int[3]);
-		Scanner user = new Scanner(System.in);
-		System.out.println("hi, please name the thermometer");
+		Scanner in = new Scanner(System.in);
 		
-		String str = user.nextLine();
-				System.out.println(str);
-		System.out.println("Enter Fahrenheit here: ");
-		int i = user.nextInt();
-		NewThermometer.setDegreesF(i);
+		System.out.println("Hi, Please name your thermometer:  ");
 		
-		System.out.println("Enter Celcius here: ");
-		int l = user.nextInt();
-		NewThermometer.setDegreesC(l);
+		String name = in.next();
+		
+		// this is how Java makes objects!!
+		Thermometer obj = new Thermometer();
+		obj.setName(name);
+		
+		System.out.println("Hi, thank you for using " + obj.getName() + " today!");
+		
+		System.out.println("Please enter your temperature in Celsius:  ");
+		int celsius = in.nextInt();
+		
+		System.out.println("Please enter your temperature in Fahrenheit:  ");
+		int fahrenheit = in.nextInt();
+		obj.setDegreesC(celsius);
+		obj.setDegreesF(fahrenheit);
+		
+		int resultsF = obj.convertToFahrenheit(celsius);
+		int resultsC = obj.convertToCelsius(fahrenheit);
+		
+		System.out.println("The temperature in " + celsius + " degrees Celsius is " + resultsF + " degrees Fahrenheit.");
+		System.out.println("The temperature in " + fahrenheit + " degrees Fahrenheit is " + resultsC + " degrees Celsius.");
+		
+		System.out.println("please enter three temperatures to calculate average: ");
+		int temp1 = in.nextInt();
+		int temp2 = in.nextInt();
+		int temp3 = in.nextInt();
+		
+		int[] temps = {temp1, temp2, temp3};
+		
+		obj.setTempReadings(temps);
+		
+		obj.calculateAvgTemp(obj.getTempReadings());
+		System.out.println("Thanks for using the app. Goodbye!");
+		//good to close all input streams like scanner to avoid memory leaks
+		in.close();
 		
 		
 	}
--- MiniProject/src/com/erictejada/code/Thermometer.java
+++ MiniProject/src/com/erictejada/code/Thermometer.java
@@ -8,18 +8,22 @@ public class Thermometer {
 	private String name;
 	private int[] tempReadings = new int[3];
 	
+	//no args constructor = a constructor that can initialize objects, object instances of this class without any parameters for any class fields.
+	//these two constructors have the same method name, but they have different number of parameters. aka method overloading. note: method overloading can also occur when there are different datatypes of parameters, but they are the same number of parameters.
 	public Thermometer() {
-		super();
+		super(); //tells the jvm that this is an object and therefore it is able to override the object classes methods: Like toString() and hashcode
 			
 		}
-	
+	//al args constructor
 	public Thermometer(int degreesF, int degreesC, String name, int[] tempReadings) {
 		this.degreesF = degreesF;
 		this.degreesC = degreesC;
 		this.tempReadings = tempReadings;
 		this.name = name;
 }
-
+//getters and setters
+// these are necessary when we are implementing encapsulation in our java code
+// as we need access to our private class fields from our main driver. 
 	public int getDegreesF() {
 		return degreesF;
 	}
@@ -51,21 +55,36 @@ public class Thermometer {
 	public void setTempReadings(int[] tempReadings) {
 		this.tempReadings = tempReadings;
 	}
-
+	//this is called method overriding, which is another form of encapsulation.
 	@Override
 	public String toString() {
 		return "Thermometer [degreesF=" + degreesF + ", degreesC=" + degreesC + ", name=" + name + ", tempReadings="
 				+ Arrays.toString(tempReadings) + "]";
 	}
-	public static double convertToFahrenheit(double c) {
-		return 5.0 / 9.0 * (convertToCelsius(0) - 32.0);
+	public static int convertToFahrenheit(int c) {
+		
+		//(0°C × 9/5) + 32 = 32°F
+		return (int)(c  * 1.8 +32);
 		
 	}
-	public static double convertToCelsius(double f){
-		return 5.0 / 9.0 * (convertToFahrenheit(0) + 32.0);
+	public static int convertToCelsius(int f){
+		
+		//(32°F − 32) × 5/9
+		return (int)((f - 32) / 1.8);
 		
 	}
 	public static void calculateAvgTemp(int[] temps) {
+		//we need a counter for the total temp numbers to start at 0
+		int total = 0;
+		//iterate thru the array
+		for(int i = 0; i < temps.length; i++) {
+		//for each temp, add it to the total number
+			total +=temps[i];
+		}
+		//end loop
 		
+		//return the total temps divided by 3
+		//
+		System.out.println("the average tempertature of these readings is " + (total/3));
 	}
 }
